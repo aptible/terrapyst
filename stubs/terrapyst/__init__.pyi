d0d5559ed3f4f923f278d8e16b0744d2198b87d5
Madhu <madhu.ravi@aptible.com>
2022-06-23 20:55:06 -0400

    trying different approach

--- stubs/terrapyst/__init__.pyi
+++
@@ -1,6 +0,0 @@
-from .authentication import TerraformAuthentication as TerraformAuthentication
-from .exceptions import TerraformError as TerraformError, TerraformRuntimeError as TerraformRuntimeError, TerraformVersionError as TerraformVersionError
-from .plan import TerraformChange as TerraformChange, TerraformPlan as TerraformPlan
-from .workspace import TerraformWorkspace as TerraformWorkspace
-
-VERSION: str
