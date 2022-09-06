0218e3bb3cd075b690884960fa94800eeb576248
Robert Hafner <tedivm@tedivm.com>
2022-09-06 12:55:13 -0500

    make pretty

--- terrapyst/__init__.py
+++ terrapyst/__init__.py
@@ -1,9 +1,9 @@
 VERSION = "0.0.1"
+from . import _version
 from .apply_log import TerraformApplyLog
 from .authentication import TerraformAuthentication
 from .exceptions import TerraformError, TerraformRuntimeError, TerraformVersionError
 from .plan import TerraformChange, TerraformPlan
 from .workspace import TerraformWorkspace
 
-from . import _version
-__version__ = _version.get_versions()['version']
+__version__ = _version.get_versions()["version"]
